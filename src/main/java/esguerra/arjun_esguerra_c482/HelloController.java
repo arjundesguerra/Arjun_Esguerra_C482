634a0e822144064c0630ff1abd6016de6aa16819
Arjun Esguerra <arjundesguerra@gmail.com>
2023-03-31 16:06:17 -0700

    first commit

---
+++ src/main/java/esguerra/arjun_esguerra_c482/HelloController.java
@@ -0,0 +1,14 @@
+package esguerra.arjun_esguerra_c482;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module esguerra.arjun_esguerra_c482 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens esguerra.arjun_esguerra_c482 to javafx.fxml;
+    exports esguerra.arjun_esguerra_c482;
+}
