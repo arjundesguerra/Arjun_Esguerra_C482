190ecde6c8cb461b676a2728620203a88381d527
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-03 18:14:25 -0700

    modify product scene and exit button

--- src/java/Controllers/MainController.java
+++ src/java/Controllers/MainController.java
@@ -1,11 +1,14 @@
 package Controllers;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,6 +43,19 @@ public class MainController {
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    public void switchToModifyProductScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("modify_product.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleExitButtonAction() {
+        Platform.exit();
     }
 
 }
---
+++ src/java/Controllers/ModifyProductController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ModifyProductController {
+
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToMainScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("main_view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
