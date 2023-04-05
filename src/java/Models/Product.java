98c5a153cedd96cad78970cc87a53588398a313f
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-04 17:14:29 -0700

    added product and inventory classes

---
+++ src/java/Models/Product.java
@@ -0,0 +1,85 @@
+package Models;
+
+import javafx.collections.ObservableList;
+
+public class Product {
+
+    private ObservableList<Part> associatedParts;
+
+    private int id;
+
+    private String name;
+
+    private double price;
+
+    private int stock;
+
+    private int min;
+
+    private int max;
+
+    public Product(int id, String name,double price, int stock, int min, int max) {
+
+    }
+
+    public void setId(int id) {
+
+    }
+
+    public void setName(String name) {
+
+    }
+
+    public void setPrice(double price) {
+
+    }
+
+    public void setStock(int stock) {
+
+    }
+
+    public void setMin(int min) {
+
+    }
+
+    public void setMax(int max) {
+
+    }
+
+    public int getId() {
+
+    }
+
+    public String getName() {
+
+    }
+
+    public double getPrice() {
+
+    }
+
+    public int getStock() {
+
+    }
+
+    public int getMin() {
+
+    }
+
+    public int getMax() {
+
+    }
+
+    public void addAssociatedPart(Part part) {
+
+    }
+
+    public boolean deleteAssociatedPart(Part selectedAssocaitedPart) {
+
+    }
+
+    public ObservableList<Part> getAllAssociatedParts() {
+
+    }
+    })
+}
