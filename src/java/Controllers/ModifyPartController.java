d10ac51023670e4242eb6fd2066eadc6a8ff1648
Arjun Esguerra <arjundesguerra@gmail.com>
2023-04-08 02:12:37 -0700

    modify view is populated with data

--- src/java/Controllers/ModifyPartController.java
+++ src/java/Controllers/ModifyPartController.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Models.InHouse;
+import Models.Outsourced;
 import Models.Part;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -15,6 +18,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ModifyPartController {
+
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField stock;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextField max;
+    @FXML
+    private TextField min;
+    @FXML
+    private TextField source;
 
     @FXML private ToggleGroup group1;
 
@@ -26,6 +44,10 @@ public class ModifyPartController {
 
     private Stage stage;
     private Scene scene;
+
+    private Part part;
+
+    private InHouse inHouse;
 
 
     public void initialize() {
@@ -43,7 +65,22 @@ public class ModifyPartController {
     }
 
     public void setPart(Part part) {
+        this.part = part;
+        name.setText(part.getName());
+        stock.setText(Integer.toString(part.getStock()));
+        price.setText(Double.toString(part.getPrice()));
+        max.setText(Integer.toString(part.getMax()));
+        min.setText(Integer.toString(part.getMin()));
 
+        if (part instanceof InHouse) {
+            inhouse.setSelected(true);
+            machineOrCompany.setText("Machine ID");
+            source.setText(Integer.toString(((InHouse) part).getMachineId()));
+        } else if (part instanceof Outsourced) {
+            outsourced.setSelected(true);
+            machineOrCompany.setText("Company Name");
+            source.setText(((Outsourced) part).getCompanyName());
+        }
     }
 
     public void switchToMainScene(ActionEvent event) throws IOException {
